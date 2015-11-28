f5b4810b056ea9f57e233f6497818eb53c926fd8
Nikhil Patel <n.patel@berkeley.edu>
2015-11-28 15:08:13 -0700

    Migrate and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+couch = Item.create(:title => "Ugly couch", :summary => "A couch", :price => "100.0", :location => "Unit 1")
+umbrella = Item.create(:title => "Umbrella", :summary => "No more rain", :price => "10.25", :location => "Unit 3")
+phone = Item.create(:title => "Old phone", :summary => "A phone", :price => "999.99", :location => "Foothill")
+Comment.create(:contents => "This is hideous!", :item => couch)
+Comment.create(:contents => "Wow that's old", :item => phone)
+Comment.create(:contents => "Doesn't even work", :item => phone)
+Comment.create(:contents => "Has a hole in it", :item => umbrella)
