414b4de3c8703c975414614bf1b00b1c39f9ab3e
Nikhil Patel <n.patel@berkeley.edu>
2015-12-03 12:05:05 -0800

    Validate all fields in User, delete Photo model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_many :photos, dependent: :destroy
 	belongs_to :user
-	validates :title, presence: true
+	validates :title, :summary, :price, :location, :category, presence: true
 	default_scope { order('created_at DESC') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # ensure that users are at least 18 years old
-  #validates :birthday, date: { after: Proc.new {Date.today.year - 18.years}}
   validate :name, :email, :password
   has_many :comments
   has_many :items
